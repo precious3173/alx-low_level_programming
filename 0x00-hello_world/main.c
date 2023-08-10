87941fb756b0ffd0af0d6043abd1b2120c8f8357
root <root@Precious>
2023-08-10 23:38:11 +0100

    Betty working

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/c/0x00$ cat main.c 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/0x00$ export CFILE=main.c
+julien@ubuntu:~/c/0x00$ ./0-preprocessor 
+julien@ubuntu:~/c/0x00$ tail c
+# 942 "/usr/include/stdio.h" 3 4
