33bf3407b44e4a5fa25daa1e975d5193748172fe
root <root@Precious>
2023-08-12 18:44:32 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - function
+*Description: 9th task
+*Return: return 0
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 16)
+{
+if (num <10)
+{
+putchar('0' + num);  
+}
+else if (num >= 10 && num <= 16){
+int last = num%10;
+int first = num/10;
+putchar('0' + first);
+putchar('0' + last);
+}
+num++;
+}
+putchar('\n');
+return (0);
+}
