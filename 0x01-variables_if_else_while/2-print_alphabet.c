6d888c2f950b5611b44e2cc0b41b6ae552e0ace2
root <root@Precious>
2023-08-12 15:48:29 +0100

    betty added to task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+/**
+*main - function
+*Description: putchar task
+*Return: returm 0
+*/
 int main()
 {
-
 char c = 'a';
-
-while (c < 'z')
+while (c <= 'z')
 {
 putchar(c);
 c++;
 }
-
 return(0);
 }
