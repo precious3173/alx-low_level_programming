683dc12ae0e2f04a10eec5836288a83b14e530f1
Precious <preciouspeter3173@gmail.com>
2023-08-12 06:18:11 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (n > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5/n", n, lastDigit);
 }
 else if (n == 0)
 {
