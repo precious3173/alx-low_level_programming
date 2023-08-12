4a42abcaf94e35246fff298bdfcc4379b7472498
root <root@Precious>
2023-08-12 17:30:05 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - function
+*Description: task 6
+*Return: return 0
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 10)
+{
+putchar('0' + num);
+num++;
+}
+putchar('\n');
+return (0);
+}
