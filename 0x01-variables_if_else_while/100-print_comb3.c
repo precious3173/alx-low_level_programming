f8c652503844160b2ef1ec15ee625a1f8b00d562
root <root@Precious>
2023-08-12 22:19:32 +0100

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int n = 1;
-while (n <= 10)
+while (n <= 89)
 {
 int first = n / 10;
 int last = n % 10;
 putchar('0' + first);
 putchar('0' + last);
-if (n < 10)
+if (n < 89)
 {
 putchar(',');
 putchar(' ');
