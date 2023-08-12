fc319e69643c41f646f3cec78fdf6aa094aa9770
root <root@Precious>
2023-08-12 16:58:14 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - function
+*Description: task 6
+*Return: retun 0
+*/
+int main(void)
+{
+int digit = 0;
+while (digit <= 10)
+{
+printf("%d", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
