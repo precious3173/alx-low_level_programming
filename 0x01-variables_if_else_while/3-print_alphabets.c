119af57d4ad809d89b1e0ac155113c7608a7a00b
root <root@Precious>
2023-08-12 16:22:13 +0100

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main -function
+*Description: fourth task
+*Return: return 0
+*/
+int main(void)
+{
+char lc = 'a';
+char uc = 'A';
+while (lc <= 'z') 
+{
+putchar(lc);
+lc++;	 
+}
+putchar('\n');
+while (uc <= 'Z')
+{
+putchar(uc);
+uc++;
+}
+putchar('\n');
+return (0);
+}
+
