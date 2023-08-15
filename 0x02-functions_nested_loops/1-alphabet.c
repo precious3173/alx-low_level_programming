84f3ab52b6c654b82a7eceafc5f852905ae4d156
Precious <preciouspeter3173@gmail.com>
2023-08-15 06:54:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void printAlphabet()
+{
+printf("_putchar\n");
+}
+int main(void)
+{
+printAlphabet();
+printf("_putchar\n");
+return (0);
+}
