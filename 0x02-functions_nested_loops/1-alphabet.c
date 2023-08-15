421e11d0922a97cf9883b07deb3193cd2416601e
Precious <preciouspeter3173@gmail.com>
2023-08-15 14:13:29 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
- int i = 97;
-     while(i <= 122)
-     {
-       _putchar(i);
-       i++;
-     } 
-     _putchar('\n');
+int i = 97;
+while (i <= 122)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
 }
