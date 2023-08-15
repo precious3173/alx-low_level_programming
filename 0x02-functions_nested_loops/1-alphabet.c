a6dc48098cd4f346b6f20613499e041d40fe9b26
Precious <preciouspeter3173@gmail.com>
2023-08-15 13:37:45 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabet to lower case
+ *
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+int i = 97;
+for (i; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
