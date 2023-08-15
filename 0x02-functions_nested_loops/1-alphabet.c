bc0cc244882444d71316015bd2751448335b0284
Precious <preciouspeter3173@gmail.com>
2023-08-15 07:45:42 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(char *string);
 int main(void)
 {
 print_alphabet("_putchar");
-printf("_putchar");
+printf("_putchar\n");
 return (0);
 }
-void print_alphabet(const char *string)
+void print_alphabet(char *string)
 {
-printf("%s", string);
+printf("%s\n", string);
 }
