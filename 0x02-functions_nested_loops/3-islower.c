b767f9e58b57e64f5a2a3fda0fe79e6d62d1f695
Precious <preciouspeter3173@gmail.com>
2023-08-16 11:23:03 -0700

    betty check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _islower - third task
+ *_islower - third task
+ *lowercase character: L
+ *0
+ *
+ * @c: The char key
+ *Return: 1
  */
 
 int _islower(int c)
 {
-  
- if (c >= 97 && c <= 122)
-{ 
+if (c >= 97 && c <= 122)
+{
 return (1);
 }
-else 
+else
 {
 return (0);
 }
 _putchar('\n');
 }
-
