a7efe29816452d50d4a7b66988c12367764a0c91
Precious <preciouspeter3173@gmail.com>
2023-08-16 11:14:08 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _islower - third task
+ */
+
+int _islower(int c)
+{
+  
+ if (c >= 97 && c <= 122)
+{ 
+return (1);
+}
+else 
+{
+return (0);
+}
+_putchar('\n');
+}
+
