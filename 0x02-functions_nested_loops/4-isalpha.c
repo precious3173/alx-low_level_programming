e3933256986c51e5e0466c970d1ca8b053163182
Precious <preciouspeter3173@gmail.com>
2023-08-16 11:40:53 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - How to take risk
+ * @c: Character
+ *
+ * Return: 1 
+ *
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
