0261143dc8b19ac5fa56f0ed6bfe30b968d7dcfd
Precious <preciouspeter3173@gmail.com>
2023-08-16 15:05:58 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,27 +5,27 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k , l;
+	int i, j, k, l;
+
 	for (i = 0; i <= 2; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
-				
 				for (l = 0; l <= 9; l++)
 				{
 					if (i >= 2 && j >= 4)
 						break;
-				_putchar(i + 48);
-				_putchar(j + 48);
-				_putchar(58);
-				_putchar(k + 48);
-				_putchar(l + 48);
-				_putchar('\n');
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar(58);
+					_putchar(k + 48);
+					_putchar(l + 48);
+					_putchar('\n');
 				}
 			}
-		   }
+		}
 	}
 
 }
