9ba4ba072684e07e34c7a433941b4107039841eb
Precious <preciouspeter3173@gmail.com>
2023-08-18 17:45:48 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - returns 0 - 9
+ * @c: third integer
+ * Return: largest number
+ */
+int _isdigit(int c)
+{
+if ( c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
