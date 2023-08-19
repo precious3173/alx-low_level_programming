71e389d4ee7eaea8faceedc1cb24669a6224c549
Precious <preciouspeter3173@gmail.com>
2023-08-18 20:09:15 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - returns the largest of 3 numbers
+ * @n: first integer
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
+}
