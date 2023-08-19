b7724ee0b4e44ec42d64a037ba7f172d1a5cf38b
Precious <preciouspeter3173@gmail.com>
2023-08-18 18:26:45 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - return void
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+for (int i = 48; i <= 57; i++)
+{
+	if (i != 50 || i != 52)
+{
+	_putchar(i);
+}
+}
+_putchar('\n');
+}
