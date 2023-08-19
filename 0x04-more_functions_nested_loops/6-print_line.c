8c9fe122623c4af5c5b1fed0b058ea8afb4587c8
Precious <preciouspeter3173@gmail.com>
2023-08-18 19:36:54 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+void print_line(int n)
+{
+int i = -4;
+
+for (; i <= n; i++)
+{
+if (i <= 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar(95);
+}
+}
+_putchar('\n');
+}
