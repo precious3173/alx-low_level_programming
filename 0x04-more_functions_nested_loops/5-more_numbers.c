8687e1c9eb225007d28c84ba3d60799be1fb8936
Precious <preciouspeter3173@gmail.com>
2023-08-18 19:18:48 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,15 @@
  */
 void more_numbers(void)
 {
-int i, j, k;
+int i, j;
 for (i = 0; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
-_putchar('1);
+_putchar('1');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
+}
 }
