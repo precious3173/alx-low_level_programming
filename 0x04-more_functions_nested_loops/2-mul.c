c0d1da2bd39ca7aebff1d4c08bb924d3cbda5b6a
Precious <preciouspeter3173@gmail.com>
2023-08-18 17:57:30 -0700

    third task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - returns multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: largest number
+ */
+
+int mul(int a, int b)
+{
+int multiple = a * b;
+return (multiple);
+}
