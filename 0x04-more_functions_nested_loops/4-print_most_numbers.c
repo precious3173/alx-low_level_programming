f49af3851b741e8d4f0be7a271ed4cd7388ba7e7
Precious <preciouspeter3173@gmail.com>
2023-08-18 18:35:32 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,12 @@
  */
 void print_most_numbers(void)
 {
-for (int i = 48; i <= 57; i++)
+int i;
+for (i = 48; i <= 57; i++)
 {
-	if (i != 50 || i != 52)
+if (i != 50 || i != 52)
 {
-	_putchar(i);
+_putchar(i);
 }
 }
 _putchar('\n');
