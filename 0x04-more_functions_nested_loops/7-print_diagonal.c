4ad48cabf434344dcdb15e0426440109eda5fa45
Precious <preciouspeter3173@gmail.com>
2023-08-20 01:51:23 -0700

    update to print_diagonal tas

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal
+ * @n: parameter
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+_putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < 1; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
