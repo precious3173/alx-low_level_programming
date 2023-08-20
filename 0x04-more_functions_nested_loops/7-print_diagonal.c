7ebfd91d2335f02a023d6016f3d664c2cc6a2798
Precious <preciouspeter3173@gmail.com>
2023-08-20 00:37:54 -0700

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,7 @@ if (n <= 0)
 {
 _putchar('\n');
 }
-else
-{
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 for (j = 0; j < i; j++)
 {
@@ -23,6 +21,5 @@ _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
-}
 }
 }
