7651aa6f1ba6a0bc1f6e570f21d9bc40d514ffc4
Precious <preciouspeter3173@gmail.com>
2023-08-20 04:36:30 -0700

    fizz buzz task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,10 +9,28 @@ int main(void)
 {
 int a;
 for (a = 1; a <= 100; a++)
+{
 if (a % 3 == 0 && a % 5 != 0)
 {
-_printf("Fizz");
+printf("Fizz");
+printf(" ");
 }
 else if ( a % 5 == 0 && a % 3 != 0)
 {
-p
+printf("Buzz");
+printf(" ");
+}
+else if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else
+{
+printf("%d", a);
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
