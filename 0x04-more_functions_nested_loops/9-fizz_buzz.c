17a8c0c64d8043e9d37ce143385c77872a2bd2f8
Precious <preciouspeter3173@gmail.com>
2023-08-20 05:14:07 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a % 3 == 0)
+if (a % 3 == 0 && a % 5 != 0)
 printf("Fizz");
-else if (a % 5 == 0)
+else if (a % 5 == 0 && a % 3 != 0)
 printf("Buzz");
-else if (a % 15 == 0)
+else if (a % 3 == 0 && a % 5 == 0)
 printf("FizzBuzz");
 else
 printf("%i", a);
