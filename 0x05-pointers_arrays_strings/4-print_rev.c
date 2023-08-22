4f5f0aa155a84be317401c3087c4bf618f926acb
Precious <preciouspeter3173@gmail.com>
2023-08-22 12:10:39 -0700

    betty fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_rev - Function 
- * @s: The parameter
- * Return: return 0
- */
+*print_rev - Function
+* @s: The parameter
+* Return: return 0
+*/
 
 void print_rev(char *s)
 {
-	
 int i, j;
 i = 0;
 while (*s != '\0')
