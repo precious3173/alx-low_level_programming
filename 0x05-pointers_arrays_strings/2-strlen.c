903b9a5db78962f9250e73c0f1735a01c42fc19f
Precious <preciouspeter3173@gmail.com>
2023-08-21 17:14:05 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function name
+ * @s: a character
+ * Return: return integer
+ */
+
+int _strlen(char *s)
+{
+  int i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+return (i);
+}
