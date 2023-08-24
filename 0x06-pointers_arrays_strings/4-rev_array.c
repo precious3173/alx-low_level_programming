0e33469cb065cfd611640cf6db5e8985b3bc4532
root <root@Precious>
2023-08-24 05:57:14 +0100

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *reverse_array - Function
+  *@a: First Parameter
+  *n: Second Parameter
+  *Return: return void
+  */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+
+for (i = 0; i < n--; i++)
+{
+j = a[i];
+a[i] = a[n];
+a[n] = t;
+}
+}
