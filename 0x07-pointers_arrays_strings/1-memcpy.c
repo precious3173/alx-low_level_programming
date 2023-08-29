3bd403f45a30060ddc160a402cdda0d1c31a0a65
root <root@Precious>
2023-08-29 20:26:16 +0100

    second task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *_memcpy - Function
+  *@dest: first parameter
+  *@src: second parameter
+  *@n: third parameter
+  *Return: return char
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int a = 0;
+int i = n;
+
+for (; a < i; a++)
+{
+dest[r] = drc[r];
+n--;
+}
+return (dest);
+}
