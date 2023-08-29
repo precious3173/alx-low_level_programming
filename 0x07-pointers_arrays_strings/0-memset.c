478b93ae3dbc56040cd9ba90f503a5c6bde07f38
root <root@Precious>
2023-08-29 19:46:26 +0100

    Read me file added

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_memset - Function
+  *@s: First Parameter
+  *@b: Second Parameter
+  *@n: Third Parameter
+  *Return: return character
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+}
